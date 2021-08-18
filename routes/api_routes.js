a108db511506e743bc502be54baea9844321fa3a
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-18 13:49:31 -0400

    clear code examples from api_routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -28,27 +28,3 @@ router.post("/notes", (req, res) => {
 /*unique ID packages: "uniqid", "uuid"*/
 
 module.exports = router;
-
-
-/*// Obtain existing reviews
-    fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedReviews = JSON.parse(data);
-  
-          // Add a new review
-          parsedReviews.push(newReview);
-  
-          // Write updated reviews back to the file
-          fs.writeFile(
-            './db/reviews.json',
-            JSON.stringify(parsedReviews, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated reviews!')
-          );
-        }
-      });*/
