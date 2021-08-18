6a75fed2d271cc1fcb53aebc88bd3070ee497e50
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-18 15:38:03 -0400

    debugging

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -3,13 +3,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+
 //* `GET /notes` should return the `notes.html` file.
-router.get('/notes', (res) => {
+router.get('./notes', (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
 //* `GET *` should return the `index.html` file.
-router.get('*', (res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
