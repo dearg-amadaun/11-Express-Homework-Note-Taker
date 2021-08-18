b1cc4c5d49411c181d41881a92e0a85b3029e37d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-18 13:45:07 -0400

    api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,23 +1,54 @@
-// Import express package
+//Dependencies
 const express = require('express');
-
-// Require the JSON file and assign it to a variable called `termData`
-const termData = require('./terms.json');
-const PORT = 3001;
-
-// Initialize our app variable by setting it to the value of express()
-const app = express();
-
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
-
-// res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api/terms', (req, res) => res.json(termData));
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
-
+const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 
 //* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+router.get("/notes", (req, res) => {
+    fs.readFile("../db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+  });
 
 //* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+router.post("/notes", (req, res) => {
+    fs.readFile("../db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        let newNote = JSON.parse(data);
+        newNote.push(req.body);
+        fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+            if(err) return err;
+            console.log("File saved successfully");
+        });
+    });
+});
+
+/*unique ID packages: "uniqid", "uuid"*/
+
+module.exports = router;
+
+
+/*// Obtain existing reviews
+    fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedReviews = JSON.parse(data);
+  
+          // Add a new review
+          parsedReviews.push(newReview);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/reviews.json',
+            JSON.stringify(parsedReviews, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated reviews!')
+          );
+        }
+      });*/
