f3c5957f0c6f21891d3ffc30251b111dc1de5755
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-18 12:46:11 -0400

    server.js remove examples

--- server.js
+++ server.js
@@ -5,26 +5,10 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
-
-
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
-
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/send.html'))
-);
-
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/paths.html'))
-);
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
 
 //Routing
 app.use('/api', api_routes);
