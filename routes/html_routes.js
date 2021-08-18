695f57792d5457853c852d0a513406002ea97ad6
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-18 15:49:25 -0400

    fixed /note error, page loads.

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -5,13 +5,13 @@ const path = require('path');
 
 
 //* `GET /notes` should return the `notes.html` file.
-router.get('./notes', (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
 //* `GET *` should return the `index.html` file.
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
-module.exports = router;
+module.exports = router; 
